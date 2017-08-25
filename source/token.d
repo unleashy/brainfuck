63fa24f3f5282db606495830a982a5e0c99eed42
unleashy <lakeavenger@gmail.com>
2017-08-25 18:08:23 -0300

    Add Token enum

---
+++ source/token.d
@@ -0,0 +1,13 @@
+enum Token
+{
+    RIGHT_ARROW   = '>',
+    LEFT_ARROW    = '<',
+    PLUS          = '+',
+    MINUS         = '-',
+    DOT           = '.',
+    COMMA         = ',',
+    RIGHT_BRACKET = '[',
+    LEFT_BRACKET  = ']',
+
+    EOF = '\0'
+}
